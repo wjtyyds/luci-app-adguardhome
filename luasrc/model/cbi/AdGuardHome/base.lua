7bd60beb39668c05dad3d2f580d343cc65b117ee
wjtyyds <1079747631@qq.com>
2026-09-23 15:17:29 +0800

    处理adguardhome v25.12.x界面翻译问题

--- luasrc/model/cbi/AdGuardHome/base.lua
+++ luasrc/model/cbi/AdGuardHome/base.lua
@@ -8,7 +8,7 @@ local configpath=uci:get("AdGuardHome","AdGuardHome","configpath") or "/etc/AdGu
 local binpath=uci:get("AdGuardHome","AdGuardHome","binpath") or "/usr/bin/AdGuardHome/AdGuardHome"
 httpport=uci:get("AdGuardHome","AdGuardHome","httpport") or "3000"
 m = Map("AdGuardHome", "AdGuard Home")
-m.description = translate("Free and open source, powerful network-wide ads & trackers blocking DNS server.") .. "，默认账号和密码均为：admin"
+m.description = translate("Free and open source, powerful network-wide ads & trackers blocking DNS server.")
 m:section(SimpleSection).template  = "AdGuardHome/AdGuardHome_status"
 
 s = m:section(TypedSection, "AdGuardHome")
